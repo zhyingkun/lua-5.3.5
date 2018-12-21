e5c04e05bcd1a9668e04f9db49c1c82f7805062a
zhyingkun <1464959348@qq.com>
2018-12-21 22:51:00 +0800

    fix shell bug with \

--- cmod/create-lua-cmod.sh
+++ cmod/create-lua-cmod.sh
@@ -82,7 +82,7 @@ source_group(src FILES \${HELLOMOD_SRC})
 add_library(\${PROJECT_NAME} MODULE \${HELLOMOD_SRC})
 target_link_libraries(\${PROJECT_NAME} lualib)
 if(WIN32)
-	set_target_properties(${PROJECT_NAME} PROPERTIES
+	set_target_properties(\${PROJECT_NAME} PROPERTIES
 		PREFIX "lib")
 endif(WIN32)
 EOF
