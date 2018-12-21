db2581361f475bb2341e75afafb7efd0f57d6845
zhyingkun <1464959348@qq.com>
2018-12-21 12:39:38 +0800

    cmd for delete lua cmod

---
+++ cmod/delete-lua-cmod.sh
@@ -0,0 +1,48 @@
+#! /usr/bin/env bash
+
+usage(){
+	echo "
+    [sudo] sh $0 <LuaCModName>
+
+        <LuaCModName>: The c module name you want to delete.
+		"
+}
+exitWhileError(){
+	if test $? -ne 0 ; then
+		echo "
+        Error Message: $1
+        Please Check The Error And Test Again! Exit Now.
+        "
+		exit
+	fi
+}
+
+if test $# -ne 1 ; then
+	usage
+	exit
+fi
+
+cmodName=`echo $1 | sed "s/\/$//"`
+# echo $cmodName
+
+if ! test -d $cmodName ; then
+	echo \"$cmodName\"" module does not exist!!! Exit Now."
+	exit
+fi
+
+echo "Delete Directories ..."
+rm -rf $cmodName
+exitWhileError "Delete Directories failed"
+
+echo "Delete features from CMakeLists.txt ..."
+hostSystem=`uname -s`
+if [ $hostSystem == "Darwin" ]; then
+	sed -i "" '/add_subdirectory('$cmodName')/d' CMakeLists.txt
+	sed -i "" '/set_property(TARGET '$cmodName' PROPERTY FOLDER "cmod")/d' CMakeLists.txt
+else
+	sed -i '/add_subdirectory('$cmodName')/d' CMakeLists.txt
+	sed -i '/set_property(TARGET '$cmodName' PROPERTY FOLDER "cmod")/d' CMakeLists.txt
+fi
+exitWhileError "Delete features from CMakeLists.txt failed"
+
+echo "All done."
