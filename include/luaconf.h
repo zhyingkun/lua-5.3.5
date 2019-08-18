f1bb86f9c67c45f5f18028adb007c3ec57bb80cd
zhyingkun <1464959348@qq.com>
2019-08-18 19:51:39 +0800

    Add local path to package.cpath default value

--- include/luaconf.h
+++ include/luaconf.h
@@ -203,11 +203,13 @@
 #define LUA_ROOT	"/usr/local/"
 #define LUA_LDIR	LUA_ROOT "share/lua/" LUA_VDIR "/"
 #define LUA_CDIR	LUA_ROOT "lib/lua/" LUA_VDIR "/"
+#define LUA_ZYK_CDIR LUA_ROOT "zyk/lib/lua/" LUA_VDIR "/"
 #define LUA_PATH_DEFAULT  \
 		LUA_LDIR"?.lua;"  LUA_LDIR"?/init.lua;" \
 		LUA_CDIR"?.lua;"  LUA_CDIR"?/init.lua;" \
 		"./?.lua;" "./?/init.lua"
 #define LUA_CPATH_DEFAULT \
+		LUA_ZYK_CDIR"?.so;" \
 		LUA_CDIR"?.so;" LUA_CDIR"loadall.so;" "./?.so"
 #endif			/* } */
 
