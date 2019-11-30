ead446fc492f55e2fe97bcbdd1dc5bc1f7ba9751
zhyingkun <1464959348@qq.com>
2019-11-30 14:57:19 +0800

    Fix table.concat error and add table.sort

--- demo/stdlib/stdlib.lua
+++ demo/stdlib/stdlib.lua
@@ -1,12 +1,13 @@
 table.concat = function(list, sep, i, j)
-	sep = sep or ""
 	i = i or 1
 	j = j or #list
+	if j < i then return "" end
+	sep = sep or ""
 	local result = ""
-	for index = i, j do
+	for index = i, j-1 do
 		result = result .. list[index] .. sep
 	end
-	return result
+	return result .. list[j]
 end
 
 table.maxn = function(list)
@@ -81,6 +82,73 @@ table.move = function(list, first, ed, start, list2)
 	return list2
 end
 
+local partition = function(list, comp, low, up)
+	local i = low
+	local j = up - 1
+	while true do
+		i = i + 1
+		local vp = list[up - 1]
+		while comp(list[i], vp) do
+			i = i + 1
+		end
+		j = j - 1
+		while comp(vp, list[j]) do
+			j = j - 1
+		end
+		if j < i then
+			list[up - 1] = list[i]
+			list[i] = vp
+			return i
+		end
+		local tmp = list[i]
+		list[i] = list[j]
+		list[j] = tmp
+	end
+end
+
+local recusiveSort
+recusiveSort = function(list, comp, low, up)
+	while low < up do
+		local vlow = list[low]
+		local vup = list[up]
+		if comp(vup, vlow) then 
+			list[low] = vup
+			list[up] = vlow
+		end
+		if (up - low) == 1 then return end
+		local p = (low + up) / 2 -- Pivot
+		local vp = list[p]
+		vlow = list[low]
+		if comp(vp, vlow) then
+			list[low] = vp
+			list[p] = vlow
+		else
+			vup = list[up]
+			if comp(vup, vp) then
+				list[p] = vup
+				list[up] = vp
+			end
+		end
+		if (up - low) == 2 then return end
+		vp = list[p]
+		local vupmo = list[up - 1] -- Value of "Up Minus One"
+		list[p] = vupmo
+		list[up-1] = vp
+		p = partition(list, comp, low, up);
+		recusiveSort(list, comp, low, p - 1);
+		low = p + 1
+	end
+end
+
+table.sort = function(list, comp)
+	local len = #list
+	comp = comp or function(a, b) return a < b end
+	print(len, comp(list[2], list[3]))
+	if len > 1 then
+		recusiveSort(list, comp, 1, len)
+	end
+end
+
 local list = {"I", "like", "you", "!"}
 table.insert(list, 2, "very")
 print(table.remove(list, #list+1))
@@ -88,3 +156,6 @@ print(table.maxn(list), table.concat(list, " "))
 
 local tmp = table.move(list, 2, 4, 1, {})
 for k,v in pairs(tmp) do print(k,v) end
+
+table.sort(list)
+print(table.maxn(list), table.concat(list, " _ "))
