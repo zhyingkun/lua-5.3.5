b267504a2a1f480bf15bfee2ac79f63fdbfad3c7
zhyingkun <1464959348@qq.com>
2019-11-30 14:55:51 +0800

    Update luatt for display all types

--- tools/luatt/src/luatt.c
+++ tools/luatt/src/luatt.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void usage(char* cmdName) {
   printf("\tusage: %s <TagTypeNumber>\n\tTagTypeNumber: Integer Number\n", cmdName);
@@ -33,37 +34,90 @@ static const char* const ActualTagNames[9] = {
     "LUA_TUSERDATA",
     "LUA_TTHREAD",
 };
-static const char* const VariantTagFunctions[3] = {"Lua Closure", "Light C Function", "C Closure"};
-static const char* const VariantTagStrings[2] = {"Short Strings", "Long Strings"};
-static const char* const VariantTagNumbers[2] = {"Float Numbers", "Integer Numbers"};
-static const char* const IsCollectable[2] = {"Not Collectable", "Collectable"};
+static const char* const VariantTagFunctions[3] = {
+    "Lua Closure",
+    "Light C Function",
+    "C Closure",
+};
+static const char* const VariantTagStrings[2] = {
+    "Short Strings",
+    "Long Strings",
+};
+static const char* const VariantTagNumbers[2] = {
+    "Float Numbers",
+    "Integer Numbers",
+};
+static const char* const IsCollectable[2] = {
+    "Not Collectable",
+    "Collectable",
+};
+static const int AllTypeNumber[13] = {
+    0, // nil
+    1, // boolean
+    2, // light userdata
+    3, // float number
+    19, // integer number
+    68, // short string
+    84, // long string
+    69, // table
+    70, // lua closure
+    22, // light c function
+    102, // c closure
+    71, // userdata
+    72, // thread
+};
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    usage(argv[0]);
-    return -1;
+bool is_type_num_valid(int type) {
+  int length = sizeof(AllTypeNumber) / sizeof(int);
+  for (int i = 0; i < length; i++) {
+    if (type == AllTypeNumber[i]) {
+      return true;
+    }
   }
-  int tagType = atoi(argv[1]);
+  return false;
+}
+
+void display_type(int tagType) {
   int actualTag = tagType & 0x0f;
   int variantBits = (tagType >> 4) & 0x03;
   int collectable = (tagType >> 6) & 0x01;
-  printf("actual tag: %s\n", ActualTagNames[actualTag]);
-  const char* variantName = NULL;
+  printf("%3d => ActualTag: %s", tagType, ActualTagNames[actualTag]);
   switch (actualTag) {
     case LUA_TFUNCTION:
-      variantName = VariantTagFunctions[variantBits];
+      printf(" VariantTag: %s", VariantTagFunctions[variantBits]);
       break;
     case LUA_TSTRING:
-      variantName = VariantTagStrings[variantBits];
+      printf(" VariantTag: %s", VariantTagStrings[variantBits]);
       break;
     case LUA_TNUMBER:
-      variantName = VariantTagNumbers[variantBits];
+      printf(" VariantTag: %s", VariantTagNumbers[variantBits]);
       break;
     default:
-      variantName = "No Variant Tag Type";
       break;
   }
-  printf("variant tag: %s\n", variantName);
-  printf("is collectable: %s\n", IsCollectable[collectable]);
+  if (collectable == 1) {
+    printf(" [%s]", IsCollectable[collectable]);
+  }
+  printf("\n");
+}
+
+void display_all_type() {
+  int length = sizeof(AllTypeNumber) / sizeof(int);
+  for (int i = 0; i < length; i++) {
+    display_type(AllTypeNumber[i]);
+  }
+}
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    usage(argv[0]);
+    display_all_type();
+    return 0;
+  }
+  int tagType = atoi(argv[1]);
+  if (is_type_num_valid(tagType) == false) {
+    fprintf(stderr, "Tag type %d is invalid!\n", tagType);
+    return 0;
+  }
+  display_type(tagType);
   return 0;
 }
