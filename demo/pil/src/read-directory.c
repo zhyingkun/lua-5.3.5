95ac3ce39eaa7ac6477e791fd7f88bb625b99675
zhyingkun <1464959348@qq.com>
2020-01-12 11:10:15 +0800

    Using read directory in windows

--- demo/pil/src/read-directory.c
+++ demo/pil/src/read-directory.c
@@ -1,16 +1,41 @@
-#include <dirent.h>
 #include <errno.h>
 #include <string.h>
 
 #include "lua.h"
 #include "lauxlib.h"
 
+#if defined(_WIN32)
+#include <io.h>
+#else
+#include <dirent.h>
+#endif
+
+#include <stdio.h>
+
 int l_dir(lua_State* L) {
+  const char* path = luaL_checkstring(L, 1);
+#if defined(_WIN32)
+  char pathWithExt[260];
+  strcpy(pathWithExt, path);
+  strcat(pathWithExt, "/*");
+  struct _finddata_t dir;
+  intptr_t fh = _findfirst(pathWithExt, &dir);
+  if (fh == -1) { /* error opening the directory? */
+    lua_pushnil(L); /* return nil... */
+    lua_pushfstring(L, "cannot open %s", path);
+    return 2; /* number of results */
+  }
+  lua_newtable(L);
+  int i = 1;
+  do {
+    lua_pushstring(L, dir.name); /* push value */
+    lua_seti(L, -2, i++); /* table[i] = entry name */
+  } while (_findnext(fh, &dir) == 0);
+  _findclose(fh);
+#else
   DIR* dir;
   struct dirent* entry;
   int i;
-  const char* path = luaL_checkstring(L, 1);
-
   /* open directory */
   dir = opendir(path);
   if (dir == NULL) { /* error opening the directory? */
@@ -18,7 +43,6 @@ int l_dir(lua_State* L) {
     lua_pushstring(L, strerror(errno)); /* and error message */
     return 2; /* number of results */
   }
-
   /* create result table */
   lua_newtable(L);
   i = 1;
@@ -27,7 +51,7 @@ int l_dir(lua_State* L) {
     lua_pushstring(L, entry->d_name); /* push value */
     lua_settable(L, -3); /* table[i] = entry name */
   }
-
   closedir(dir);
+#endif
   return 1; /* table is already on top */
 }
