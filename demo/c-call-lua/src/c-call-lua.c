993fe8cb9e8d2a37d792d533d3e08847445f6a3f
zhyingkun <1464959348@qq.com>
2020-01-18 22:42:18 +0800

    Using lua_ident in windows

--- demo/c-call-lua/src/c-call-lua.c
+++ demo/c-call-lua/src/c-call-lua.c
@@ -416,9 +416,8 @@ int main(int argc, char const* argv[]) {
     return EXIT_FAILURE;
   }
   luaL_openlibs(L);
-#if !defined(_WIN32)
+
   printf("lua_ident: %s\n", lua_ident);
-#endif
   printf("top: %d, L->stack: %p, L->top: %p\n", lua_gettop(L), L->stack, L->top);
 
   print_lua_string_table(L);
