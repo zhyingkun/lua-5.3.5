58cd6032cd29fb8bf76ceb4e7099445a39639697
zhyingkun <1464959348@qq.com>
2020-02-24 21:26:48 +0800

    Make miniforth a module

--- demo/lpg/miniforth.lua
+++ demo/lpg/miniforth.lua
@@ -1,109 +1,189 @@
-subj = "5 DUP * ."
-pos = 1
+-------------------------------------------------------------------------------
+-- parser
+-------------------------------------------------------------------------------
+local parser = {
+    subj = "",
+    pos = 1,
+}
 
-function parsebypattern(pat)
-    local capture, newpos = string.match(subj, pat, pos)
+function parser:setinput(subj)
+    self.subj = subj
+    self.pos = 1
+end
+
+function parser:parsebypattern(pat)
+    local capture, newpos = string.match(self.subj, pat, self.pos)
     if newpos then
-        pos = newpos
+        self.pos = newpos
         return capture
     end
 end
 
-what2pattern = {
+local what2pattern = {
     spaces = "^([ \t]*)()",
     word = "^([^ \t\n]+)()",
     newline = "^(\n)()",
     restofline = "^([^\n]*)()"
 }
 
-function parse(what)
-    return parsebypattern(what2pattern[what])
+function parser:parse(what)
+    return self:parsebypattern(what2pattern[what])
 end
 
-function getword()
-    parse("spaces")
-    return parse("word")
+function parser:getword()
+    self:parse("spaces")
+    return self:parse("word")
 end
 
-function getwordornewline()
-    return getword() or parse("newline")
+function parser:getwordornewline()
+    return self:getword() or self:parse("newline")
 end
 
-_F = {}
-function eval(str) load(str)() end
-_F["%L"] = function() eval(parse("restofline")) end
+-------------------------------------------------------------------------------
+-- _F, which store all words
+-------------------------------------------------------------------------------
+local _F = {
+    ["%L"] = function()
+        return function(modes, _F, parser)
+            load(parser:parse("restofline"))()(modes, _F, parser)
+        end
+    end
+}
 
-mode = "interpret"
-modes = {}
+-------------------------------------------------------------------------------
+-- Internal stack
+-------------------------------------------------------------------------------
+local stack = {
+    DS = {}, -- stack
+    n = 0, -- length
+}
 
-function run(subjj)
-    subj = subjj
-    pos = 1
-    mode = "interpret"
-    while mode ~= "stop" do
-        modes[mode]()
+function stack:push(x)
+    local n = self.n
+    n = n + 1
+    self.n = n
+    self.DS[n] = x
+end
+
+function stack:pop()
+    local n = self.n
+    local x = self.DS[n]
+    self.DS[n] = nil
+    n = n - 1
+    self.n = n
+    return x
+end
+
+-------------------------------------------------------------------------------
+-- modes, include all interpret
+-------------------------------------------------------------------------------
+local modes = {
+    mode = "interpret",
+    parser = parser,
+    _F = _F, -- store all words
+    stack = stack,
+}
+
+function modes:setmode(mode)
+    self.mode = mode
+end
+
+function modes:getmode()
+    return self.mode
+end
+
+function modes:islive()
+    return self:getmode() ~= "stop"
+end
+
+function modes:runmode()
+    self[self.mode](self)
+end
+
+function modes:interpretprimitive(word)
+    local f = self._F[word]
+    if type(f) == "function" then
+        f()(self, self._F, self.parser)
+        return true
+    else
+        return false
+    end
+end
+
+function modes:interpretnonprimitive(word)
+    return false
+end
+
+function modes:interpretnumber(word)
+    local num = tonumber(word)
+    if num then
+        self.stack:push(num)
+        return true
+    else
+        return false
+    end
+end
+
+function modes:printstate(word)
+end
+
+function modes:interpret()
+    local word = self.parser:getwordornewline() or ""
+    self:printstate(word)
+    if self:interpretprimitive(word) then
+        return
+    end
+    if self:interpretnonprimitive(word) then
+        return
+    end
+    if self:interpretnumber(word) then
+        return
+    end
+    error("Can't interpret: " .. word)
+end
+
+-------------------------------------------------------------------------------
+-- Runtime Insert Word
+-------------------------------------------------------------------------------
+local defsubj = [=[
+    %L return function(modes, _F, parser) _F["\n"] = function() return function(modes, _F, parser) end end end
+    %L return function(modes, _F, parser) _F[""] = function() return function(modes, _F, parser) modes:setmode("stop") end end end
+    %L return function(modes, _F, parser) _F["[L"] = function() return function(modes, _F, parser) load(parser:parsebypattern("^(.-)%sL]()"))()(modes, _F, parser) end end end
+    [L
+        return function(modes, _F, parser)
+            _F["DUP"] = function()
+                return function(modes, _F, parser)
+                    local stack = modes.stack
+                    local num = stack:pop()
+                    stack:push(num)
+                    stack:push(num)
+                end
+            end
+            _F["*"] = function()
+                return function(modes, _F, parser)
+                    local stack = modes.stack
+                    stack:push(stack:pop() * stack:pop())
+                end
+            end
+            _F["."] = function()
+                return function(modes, _F, parser)
+                    io.write(" " .. modes.stack:pop())
+                end
+            end
+        end
+    L]
+]=]
+
+local function run(subj)
+    parser:setinput(subj)
+    modes:setmode("interpret")
+    while modes:islive() do
+        modes:runmode()
     end
     io.write("\n")
 end
 
-word = ""
+run(defsubj)
+-- run("5 DUP * .")
 
-function interpretprimitive()
-    local f = _F[word]
-    if type(f) == "function" then
-        f()
-        return true
-    end
-end
-
-function interpretnonprimitive()
-    return false
-end
-
-function interpretnumber()
-    local num = tonumber(word)
-    if num then
-        _F[word] = function() push(DS, num) end
-        interpretprimitive()
-        return true
-    end
-end
-
-function printstate()
-end
-
-function modes.interpret()
-    word = getwordornewline() or ""
-    printstate()
-    local _ = interpretprimitive() or interpretnonprimitive() or interpretnumber() or error("Can't interpret: " .. word)
-end
-
-mysubj1 = [=[
-    %L _F["\n"] = function() end
-    %L _F[""] = function() mode = "stop" end
-    %L _F["[L"] = function() eval(parsebypattern("^(.-)%sL]()")) end
-    [L
-        DS = { n = 0 }
-        push = function(stack, x)
-            local n = stack.n
-            n = n + 1
-            stack.n = n
-            stack[n] = x
-        end
-        pop = function(stack)
-            local n = stack.n
-            local x = stack[n]
-            stack[n] = nil
-            n = n - 1
-            stack.n = n
-            return x
-        end
-        _F["5"] = function() push(DS, 5) end
-        _F["DUP"] = function() push(DS, DS[DS.n]) end
-        _F["*"] = function() push(DS, pop(DS) * pop(DS)) end
-        _F["."] = function() io.write(" " .. pop(DS)) end
-    L]
-]=]
-
-run(mysubj1)
-run("5 DUP * .")
+return run
