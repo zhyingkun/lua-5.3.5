94afeeda35a9321f9c7ef2d20713f62f96a0c1e8
zhyingkun <1464959348@qq.com>
2020-03-05 22:26:40 +0800

    Update test code for gb2312 map

--- cmod/gb2312/test/gb2312-test.c
+++ cmod/gb2312/test/gb2312-test.c
@@ -6,17 +6,46 @@ int main(int argc, const char* argv[]) {
   (void)argc;
   (void)argv;
   unsigned int gbcode = 0;
+  size_t nullcount = 0;
+  size_t validcount = 0;
+  size_t symcount = 0;
+  size_t chinese = 0;
+  // First byte: [0xA1, 0xA9] and [0xB0, 0xF7], Second byte: [0xA1, 0xFE]
+  // 0xA1 => 0
+  // 0xA9 => 8
   for (size_t i = 0; i < 94 - 7 - 6; i++) {
     for (size_t j = 0; j < 94; j++) {
+      size_t idx = i * 94 + j;
+      if (GB2312_Unicode[idx].unicode == 0x0000) {
+        nullcount++;
+      } else {
+        validcount++;
+        if (i < 9) {
+          symcount++;
+        } else {
+          chinese++;
+        }
+      }
       if (j == 0) {
-        gbcode = GB2312_Unicode[i * 94 + j].gb2312;
+        gbcode = GB2312_Unicode[idx].gb2312;
+        unsigned int section = i + (i > 8 ? 6 : 0) + 0xA1;
+        assert(gbcode == ((section << 8) | 0xA1));
         continue;
       }
-      unsigned int next = GB2312_Unicode[i * 94 + j].gb2312;
+      unsigned int next = GB2312_Unicode[idx].gb2312;
       assert(next == gbcode + 1);
       gbcode = next;
     }
   }
+  assert(GB2312_Unicode_Size == 81 * 94);
+  assert(nullcount + validcount == GB2312_Unicode_Size);
+  assert(symcount + chinese == validcount);
+  printf("All: %zu, Valid: %zu, NULL: %zu, Symbol: %zu, Chinese: %zu\n",
+         GB2312_Unicode_Size,
+         validcount,
+         nullcount,
+         symcount,
+         chinese);
   printf("GB2312 map is OK!\n");
   return 0;
 }
