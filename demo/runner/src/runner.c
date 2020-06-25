0a525b6a7ecf836e3a2b4266ade27b89b08e447c
zhyingkun <1464959348@qq.com>
2020-06-25 22:52:24 +0800

    Update runner for main script

--- demo/runner/src/runner.c
+++ demo/runner/src/runner.c
@@ -1,12 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
 
-static const char* script = "./scripts/run.lua";
+#if defined(_WIN32)
+#include <windows.h>
+#define PATH_MAX MAX_PATH
+#else
+#endif
+
+static char script[PATH_MAX + 1];
+
+#if defined(_WIN32)
+#define DEFAULT_SCRIPT "scripts\\run.lua"
+static void resolve_absolute_path() {
+  char* lb;
+  DWORD nsize = sizeof(script) / sizeof(char);
+  DWORD n = GetModuleFileNameA(NULL, script, nsize); /* get exec. name */
+  if (n == 0 || n == nsize || (lb = strrchr(script, '\\')) == NULL) {
+    fprintf(stderr, "unable to get ModuleFileName, use default path\n");
+    fflush(stderr);
+    getcwd(script, nsize);
+    strcat(script, "\\");
+  } else {
+    *(++lb) = '\0';
+  }
+}
+#else
+#define DEFAULT_SCRIPT "scripts/run.lua"
+static void resolve_absolute_path(const char* runner) {
+  if (*runner != '/') {
+    getcwd(script, sizeof(script) / sizeof(char));
+    strcat(script, "/");
+    strcat(script, runner);
+  } else {
+    strcpy(script, runner);
+  }
+  char* lb = strrchr(script, '/');
+  *(++lb) = '\0';
+}
+#endif
 
 static int pmain(lua_State* L) {
+  int argc = lua_tointeger(L, 1);
+  char** argv = (char**)lua_topointer(L, 2);
+
+  lua_createtable(L, argc - 1, 1);
+  for (int i = 0; i < argc; i++) {
+    lua_pushstring(L, argv[i]);
+    lua_seti(L, -2, i);
+    // printf("%d : %s\n", i, argv[i]);
+  }
+  lua_setglobal(L, "arg");
+
+#if defined(_WIN32)
+  resolve_absolute_path();
+#else
+  resolve_absolute_path(argv[0]);
+#endif
+  if (chdir(script) != 0) {
+    fprintf(stderr, "chdir error: %s, path: %s\n", strerror(errno), script);
+    fflush(stderr);
+    return 0;
+  }
+  strcat(script, DEFAULT_SCRIPT);
+  printf("main script: %s\n", script);
+
   int status = luaL_dofile(L, script);
   if (status != LUA_OK) {
     const char* msg = lua_tolstring(L, -1, NULL);
