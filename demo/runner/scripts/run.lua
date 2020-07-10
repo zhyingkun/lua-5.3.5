5e7c4df8f9d7422e37c71f14fcb518f8625479fa
zhyingkun <1464959348@qq.com>
2020-07-10 22:03:33 +0800

    Update runner script

--- demo/runner/scripts/run.lua
+++ demo/runner/scripts/run.lua
@@ -1,5 +1,6 @@
 local cpath
-if os.sysname == "Windows" then
+local isWin = os.sysname == "Windows"
+if isWin then
 	cpath = ".\\cmods\\?.dll;"
 else
 	cpath = "./cmods/?.so;"
@@ -11,8 +12,34 @@ if os.sysname == "Windows" then
 	exe = exe:sub(1, -5)
 end
 
-if exe == "runner" then
-	print("Hello runner")
-elseif exe == "task1" then
-	print("Hello task1")
+local tasks = {
+	runner = function()
+		print("Good Good Study, Day Day Up!")
+	end,
+	task1 = function()
+		require("scripts/task1")
+	end,
+}
+
+local function run()
+	local task = tasks[exe]
+	if type(task) ~= "function" then
+		task = loadfile("scripts/" .. exe .. ".lua")
+	end
+	if type(task) == "function" then
+		task()
+	else
+		error("No task to run!")
+	end
+end
+
+print("Start ...")
+local ret, msg = pcall(run)
+if not ret then
+	print(debug.traceback(msg))
+end
+print("End.")
+
+if isWin then
+	os.execute("pause")
 end
