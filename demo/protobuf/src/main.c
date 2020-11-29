5f792aa726b44edd7243e50125ca6a00c628b954
zhyingkun <1464959348@qq.com>
2020-11-29 23:43:11 +0800

    Add demo for protobuf

---
+++ demo/protobuf/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <pbc.h>
+
+int main(int argc, char* argv[]) {
+  (void)argc;
+  (void)argv;
+  printf("Hello protobuf demo\n");
+  return 0;
+}
