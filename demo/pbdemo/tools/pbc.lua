705d3a20807ef89e294f883668a5a59d42e11a59
zhyingkun <1464959348@qq.com>
2021-01-07 22:59:30 +0800

    For output formatted proto

--- demo/pbdemo/tools/pbc.lua
+++ demo/pbdemo/tools/pbc.lua
@@ -1,10 +1,16 @@
 local pbc = require("libprotobuf")
 local ParseVarint = require("varint")
 
+local Label = {
+	[0] = "optional",
+	"required",
+	"repeated"
+}
+
 local PBCFieldConfig = {
 	{ "name", false, nil },
 	{ "id", false, nil },
-	{ "label", false, nil },
+	{ "label", false, function(field) return Label[field] end },
 	{ "type", false, nil },
 	{ "type_name", false, nil },
 	{ "default_int", false, nil },
@@ -29,4 +35,60 @@ local msg = fd:read("a")
 fd:close()
 fd = nil
 local fieldTbl = ParseVarint(msg, PBCFileConfig)
-print(tostring(fieldTbl, 16))
+
+local TypeName = {
+	"double",
+	"float",
+	"int64",
+	"uint64",
+	"int32",
+	"fixed64",
+	"fixed32",
+	"bool",
+	"string",
+	nil,
+	nil,
+	"bytes",
+	"uint32",
+	nil,
+	"sfixed32",
+	"sfixed64",
+	"sint32",
+	"sint64",
+}
+
+local outputTbl = {}
+local insert = table.insert
+local function OutputLine(str)
+	insert(outputTbl, str)
+end
+
+local idx = 0
+for index, name in ipairs(fieldTbl.enum_name) do
+	OutputLine("enum " .. name:sub(1, -2) .. " {")
+	for i = 1, fieldTbl.enum_size[index], 1 do
+		idx = idx + 1
+		local str = fieldTbl.enum_string[idx]:sub(1, -2)
+		local id = fieldTbl.enum_id[idx]
+		OutputLine("\t" .. str .. " = " .. tostring(id) .. ";")
+	end
+	OutputLine("}")
+end
+
+local idx = 0
+for index, name in ipairs(fieldTbl.message_name) do
+	OutputLine("message " .. name:sub(1, -2) .. " {")
+	for i = 1, fieldTbl.message_size[index], 1 do
+		idx = idx + 1
+		local field = fieldTbl.message_field[idx]
+		local typeName = TypeName[field.type]
+		if typeName == nil then
+			typeName = field.type_name:match(".*%.(.*).$")
+		end
+		local statement = "\t" .. field.label .. " " .. typeName .. " " .. field.name:sub(1, -2) .. " = " .. tostring(field.id) .. ";"
+		OutputLine(statement)
+	end
+	OutputLine("}")
+end
+
+print(table.concat(outputTbl, "\n"))
