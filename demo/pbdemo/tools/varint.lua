b3a2e78e320f8a378f6b8be54d89fb8ff1aace69
zhyingkun <1464959348@qq.com>
2021-01-09 15:57:19 +0800

    Update varint processing

--- demo/pbdemo/tools/varint.lua
+++ demo/pbdemo/tools/varint.lua
@@ -3,13 +3,14 @@ local pbc = require("libprotobuf")
 local useNumber = false
 local function ParseVarint(msg, config)
 	local fieldTbl = {}
+	local insert = table.insert
 	for _, number, wiretype, field in pbc.varints(msg) do
 		local fieldConfig = config[number]
 		assert(type(fieldConfig) == "table")
 		local key = useNumber and number or fieldConfig[1]
 		local bIsArray = fieldConfig[2]
 		local process = fieldConfig[3]
-		local value = field
+		local value
 		local tProcess = type(process)
 		if tProcess == "function" then
 			value = process(field)
@@ -17,14 +18,14 @@ local function ParseVarint(msg, config)
 			value = ParseVarint(field, process)
 		else
 			assert(tProcess == "nil")
+			value = field
 		end
 		local oldField = fieldTbl[key]
 		if oldField == nil then
 			fieldTbl[key] = bIsArray and { value } or value
 		else
-			assert(bIsArray)
-			assert(type(oldField) == "table")
-			table.insert(oldField, value)
+			assert(bIsArray and type(oldField) == "table")
+			insert(oldField, value)
 		end
 	end
 	return fieldTbl
