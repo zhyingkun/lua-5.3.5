365b6dad5598e78b73c6b12965e92af3d109f3e3
zhyingkun <1464959348@qq.com>
2021-02-13 12:02:17 +0800

    Tools for raw decode varint binary

---
+++ demo/pbdemo/tools/decoderaw.lua
@@ -0,0 +1,12 @@
+local file = arg[1]
+if not file then
+	print("pass varint binary file path in first parament")
+	return
+end
+local fd = io.open(file, "rb")
+local msg = fd:read("a")
+fd:close()
+fd = nil
+
+local fieldTbl = require("varint").ParseVarintRaw(msg)
+print(tostring(fieldTbl, 16))
