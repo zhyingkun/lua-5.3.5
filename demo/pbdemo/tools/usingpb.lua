43c20bd049357a62ebcf496f620aa90c276ce10f
zhyingkun <1464959348@qq.com>
2021-05-23 21:49:46 +0800

    Escape some files

--- demo/pbdemo/tools/usingpb.lua
+++ demo/pbdemo/tools/usingpb.lua
@@ -1,8 +1,19 @@
 local protobuf = require("protobuf.protobuf")
 local libdir = require("libdir")
 
+local EscapeFiles = {
+	"decompile.pb",
+}
+local function ShouldEscape(file)
+	for _, name in ipairs(EscapeFiles) do
+		if name == file then return true end
+	end
+	return false
+end
+
 for name, isdir in libdir.dirs("./pb") do
-	if not isdir and name:find(".pb$") then
+	-- decompile.pb depend on other pb
+	if not isdir and name:find(".pb$") and not ShouldEscape(name) then
 		-- print("zykTest:", name)
 		protobuf.register_file("./pb/" .. name)
 	end
