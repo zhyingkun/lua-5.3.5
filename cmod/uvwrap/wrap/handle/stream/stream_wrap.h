f4385878a26f8438f76cc9c07e1eb3b0b6e39c0a
zhyingkun <1464959348@qq.com>
2021-08-01 00:19:41 +0800

    Delete unused files

--- cmod/uvwrap/wrap/handle/stream/stream_wrap.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UV_STREAM_WRAP_H_
-#define _UV_STREAM_WRAP_H_
-
-#define UVWRAP_STREAM_TYPE "uv_stream_t*"
-
-LUAI_DDEC void uvwrap_stream_init_metatable(lua_State* L);
-
-#endif /* _UV_STREAM_WRAP_H_ */
--- cmod/uvwrap/wrap/handle/stream/tcp_wrap.h
+++
@@ -1,9 +0,0 @@
-#ifndef _UV_TCP_WRAP_H_
-#define _UV_TCP_WRAP_H_
-
-#define UVWRAP_TCP_TYPE "uv_tcp_t*"
-
-LUAI_DDEC int uvwrap_tcp_new(lua_State* L);
-LUAI_DDEC void uvwrap_tcp_init_metatable(lua_State* L);
-
-#endif /* _UV_TCP_WRAP_H_ */
