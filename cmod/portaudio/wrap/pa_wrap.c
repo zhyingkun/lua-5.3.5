9279f16f74a20ff851f418b35984950bcd141b36
zhyingkun <1464959348@qq.com>
2021-09-19 12:20:10 +0800

    Features for PortAudio

---
+++ cmod/portaudio/wrap/pa_wrap.c
@@ -0,0 +1,16 @@
+/*
+Enums:
+
+1. ErrorCode
+2. DeviceIndex
+3. HostApiTypeId
+4. SampleFormat
+5. StreamFlags
+6. StreamCallbackFlags
+7. StreamCallbackResult
+
+IndependenceStruct：
+
+1. StreamParameters
+2. StreamCallbackTimeInfo
+*/
