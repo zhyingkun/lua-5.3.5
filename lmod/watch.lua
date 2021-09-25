ec5d22add9d594fa0a53b39e0ffa09e61b009e38
zhyingkun <1464959348@qq.com>
2021-09-25 22:30:38 +0800

    Support for unwatch

--- lmod/watch.lua
+++ lmod/watch.lua
@@ -11,11 +11,22 @@ local function error_msg(...)
 	error(string.format(...))
 end
 
+local cache_handles = {}
+
+local function unwatch(mod_name)
+	local handle = cache_handles[mod_name]
+	if handle then
+		handle:close()
+		cache_handles[mod_name] = nil
+	end
+end
+
 local function watch(mod_name, callback)
 	local filepath = package.searchpath(mod_name, package.path)
 	if not filepath then
 		error_msg("Error Could not find lua module: %s", mod_name)
 	end
+	if cache_handles[mod_name] then return end
 	local handle = fs_event.new()
 	handle:start(function(filename, events, status)
 		if status == OK then
@@ -30,17 +41,21 @@ local function watch(mod_name, callback)
 				error_msg("Error RENAME filepath: %s, filename: %s", filepath, filename)
 			end
 		else
+			local handle_str = tostring(handle)
+			unwatch(mod_name)
 			error_msg(
 				"Watch filepath error: %s, filename: %s, handle: %s, status: %s",
 				filepath,
 				filename,
-				tostring(handle),
+				handle_str,
 				tostring(status)
 			)
 		end
 	end, filepath, RECURSIVE)
+	cache_handles[mod_name] = handle
 end
 
 return {
+	unwatch = unwatch,
 	watch = watch,
 }
