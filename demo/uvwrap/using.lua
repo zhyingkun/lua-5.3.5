b954e686fbbfa47471ee7f0e47189dcf4025718c
zhyingkun <1464959348@qq.com>
2021-10-02 12:52:21 +0800

    Demo for using libuv

---
+++ demo/uvwrap/using.lua
@@ -0,0 +1,11 @@
+local function copyfile(from, to)
+	local libuv = require("libuv")
+	local fs = libuv.fs
+	local open_flag = fs.open_flag
+	local fd_from = fs.open(from, open_flag.RDONLY, 0)
+	local fd_to = fs.open(to, open_flag.WRONLY | open_flag.CREAT, 0o644)
+	local len, str = fs.read(fd_from, -1) -- -1 means ignore offset
+	fs.close(fd_from)
+	local len = fs.write(fd_to, str, -1)
+	fs.close(fd_to)
+end
