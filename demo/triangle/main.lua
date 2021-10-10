0ee1fabf1a61f36830d3ff3aa5333982acf492b8
zhyingkun <1464959348@qq.com>
2021-10-10 17:39:33 +0800

    Move triangle rendering to a module

--- demo/triangle/main.lua
+++ demo/triangle/main.lua
@@ -59,133 +59,14 @@ bcfx.setMiscFuncs(
 
 bcfx.init(window)
 
-local layout = bcfx.vertexlayout.new()
-layout:add(vertex_attrib.Position, 3, attrib_type.Float, false)
-local layoutHandle = bcfx.createVertexLayout(layout)
-local vertexTbl = {
-	-0.6, -0.6, 0.0,
-	0.6, -0.6, 0.0,
-	0.0, 0.6, 0.0,
-}
-local ptr, sz = bcfx.MakeFloatArray(vertexTbl, #vertexTbl)
-local vertexHandle = bcfx.createVertexBuffer(ptr, sz, layoutHandle)
-
-local layoutColor = bcfx.vertexlayout.new()
-layoutColor:add(vertex_attrib.Color0, 3, attrib_type.Float, false)
-local layoutColorHandle = bcfx.createVertexLayout(layoutColor)
-local colorTbl = {
-	1.0, 0.0, 0.0,
-	0.0, 1.0, 0.0,
-	0.0, 0.0, 1.0,
-}
-local ptr, sz = bcfx.MakeFloatArray(colorTbl, #colorTbl)
-local colorHandle = bcfx.createVertexBuffer(ptr, sz, layoutColorHandle)
-
-local indexTbl = {
-	0, 1, 2,
-}
-local ptr, sz = bcfx.MakeUintArray(indexTbl, #indexTbl)
-local idxHandle = bcfx.createIndexBuffer(ptr, sz)
-
-local vsStr = [[
-#version 410 core
-in vec3 a_position;
-in vec3 a_color0;
-
-out vec3 v_color0;
-
-void main() {
-    gl_Position = vec4(a_position, 1.0);
-	v_color0 = a_color0;
-}
-]]
-local fsStr = [[
-#version 410 core
-in vec3 v_color0;
-
-out vec4 FragColor;
-
-void main() {
-	FragColor = vec4(v_color0, 1.0);
-}
-]]
-local vertexShaderHandle = bcfx.createShader(vsStr, shader_type.Vertex)
-local fragmentShaderHandle = bcfx.createShader(fsStr, shader_type.Fragment)
-local shaderProgramHandle = bcfx.createProgram(vertexShaderHandle, fragmentShaderHandle)
-
-local viewID = 0
--- bcfx.setViewWindow(viewID, window)
-local color = bcfx.color.pack(51, 76, 76, 255)
-bcfx.setViewClear(viewID, clear_flag.COLOR, color, 0.0, 0)
-bcfx.setViewRect(viewID, 0, 0, glfw.GetFramebufferSize(window))
--- bcfx.setViewRect(viewID, 0, 0, glfw.GetWindowSize(window))
-
-local viewID1 = 1
-bcfx.setViewWindow(viewID1, window)
-local color = bcfx.color.pack(255, 255, 76, 255)
-bcfx.setViewClear(viewID1, clear_flag.COLOR, color, 0.0, 0)
-bcfx.setViewRect(viewID1, 0, 0, 600, 600)
-
-glfw.SetFramebufferSizeCallback(window, function(window, width, height)
-	bcfx.setViewRect(viewID, 0, 0, width, height)
-end)
-
-
-local window2 = glfw.CreateWindow(800, 600, "Another", nil, window)
-if not window2 then
-	print("GLFW CreateWindow Error:", glfw.GetError())
-	glfw.Terminate()
-	return
-end
-
-local viewID2 = 2
-bcfx.setViewWindow(viewID2, window2)
-local color = bcfx.color.pack(128, 0, 0, 255)
-bcfx.setViewClear(viewID2, clear_flag.COLOR, color, 0.0, 0)
-bcfx.setViewRect(viewID2, 0, 0, glfw.GetFramebufferSize(window2)) -- 
-
-glfw.SetFramebufferSizeCallback(window2, function(window, width, height)
-	bcfx.setViewRect(viewID2, 0, 0, width, height)
-end)
-
-local FrameRate = 0.0
-local function GetFrameRate()
-	return FrameRate
-end
-local AddOneFrame = (function()
-	local FrameCount = 0
-	local TotalTime = 0.0
-	local MaintainFrameTime = (function()
-		local TimeCache = {}
-		local TimeIdx = 1
-		local MAX_FRAME_COUNT = 30
-		for i = 1, MAX_FRAME_COUNT, 1 do
-			TimeCache[i] = 0.0
-		end
-		return function(delta)
-			local Cache = TimeCache[TimeIdx]
-			TimeCache[TimeIdx] = delta
-			TotalTime = TotalTime - Cache + delta
-			if FrameCount < MAX_FRAME_COUNT then
-				FrameCount = FrameCount + 1
-			end
-			TimeIdx = TimeIdx % MAX_FRAME_COUNT + 1 -- [1, 30]
-		end
-	end)()
-	return function(delta)
-		MaintainFrameTime(delta)
-		FrameRate = FrameCount / TotalTime
-	end
-end)()
-
-local timer = libuv.timer.new()
-timer:start(function()
-	print("FrameRate:", GetFrameRate(), "Tick:", os.clock())
-end, 1000, 1000)
-
 libuv.repl_start()
+
+local triangle = require("triangle")
+triangle.setup(window)
 local colorcircle = require("colorcircle")
 colorcircle.setup(window)
+
+require("watch").watch("triangle")
 
 local lastTime = 0.0
 while not glfw.WindowShouldClose(window) do
@@ -196,27 +77,10 @@ while not glfw.WindowShouldClose(window) do
 	libuv.run_nowait()
 
 	local time = glfw.GetTime()
-	AddOneFrame(time - lastTime)
+	local delta = time - lastTime
 	lastTime = time
 
-	bcfx.setVertexBuffer(0, vertexHandle)
-	bcfx.setVertexBuffer(1, colorHandle)
-	bcfx.setIndexBuffer(idxHandle)
-
-	bcfx.submit(viewID, shaderProgramHandle)
-
-	bcfx.setVertexBuffer(0, vertexHandle)
-	bcfx.setVertexBuffer(1, colorHandle)
-	bcfx.setIndexBuffer(idxHandle)
-
-	bcfx.submit(viewID1, shaderProgramHandle)
-
-	bcfx.setVertexBuffer(0, vertexHandle)
-	bcfx.setVertexBuffer(1, colorHandle)
-	bcfx.setIndexBuffer(idxHandle)
-
-	bcfx.submit(viewID2, shaderProgramHandle)
-
+	triangle.tick(delta)
 	colorcircle.tick(delta)
 
 	bcfx.apiFrame()
---
+++ demo/triangle/triangle.lua
@@ -0,0 +1,190 @@
+local libuv = require("libuv")
+local glfw = require("glfw")
+
+local bcfx = require("bcfx")
+local clear_flag = bcfx.clear_flag
+local vertex_attrib = bcfx.vertex_attrib
+local attrib_type = bcfx.attrib_type
+local shader_type = bcfx.shader_type
+
+local FrameRate = 0.0
+local function GetFrameRate()
+	return FrameRate
+end
+local AddOneFrame = (function()
+	local FrameCount = 0
+	local TotalTime = 0.0
+	local MaintainFrameTime = (function()
+		local TimeCache = {}
+		local TimeIdx = 1
+		local MAX_FRAME_COUNT = 30
+		for i = 1, MAX_FRAME_COUNT, 1 do
+			TimeCache[i] = 0.0
+		end
+		return function(delta)
+			local Cache = TimeCache[TimeIdx]
+			TimeCache[TimeIdx] = delta
+			TotalTime = TotalTime - Cache + delta
+			if FrameCount < MAX_FRAME_COUNT then
+				FrameCount = FrameCount + 1
+			end
+			TimeIdx = TimeIdx % MAX_FRAME_COUNT + 1 -- [1, 30]
+		end
+	end)()
+	return function(delta)
+		MaintainFrameTime(delta)
+		FrameRate = FrameCount / TotalTime
+	end
+end)()
+
+local function CreateVertexBuffer()
+	local layout = bcfx.vertexlayout.new()
+	layout:add(vertex_attrib.Position, 3, attrib_type.Float, false)
+	local layoutHandle = bcfx.createVertexLayout(layout)
+	local vertexTbl = {
+		-0.8, -0.8, 0.0,
+		0.8, -0.8, 0.0,
+		0.0, 0.8, 0.0,
+	}
+	local ptr, sz = bcfx.MakeFloatArray(vertexTbl, #vertexTbl)
+	local vertexHandle = bcfx.createVertexBuffer(ptr, sz, layoutHandle)
+	return layout, layoutHandle, vertexHandle
+end
+local function CreateColorBuffer()
+	local layout = bcfx.vertexlayout.new()
+	layout:add(vertex_attrib.Color0, 3, attrib_type.Float, false)
+	local layoutHandle = bcfx.createVertexLayout(layout)
+	local colorTbl = {
+		1.0, 0.0, 0.0,
+		0.0, 1.0, 0.0,
+		0.0, 0.0, 1.0,
+	}
+	local ptr, sz = bcfx.MakeFloatArray(colorTbl, #colorTbl)
+	local colorHandle = bcfx.createVertexBuffer(ptr, sz, layoutHandle)
+	return layout, layoutHandle, colorHandle
+end
+local function CreateIndexBuffer()
+	local indexTbl = {
+		0, 1, 2,
+	}
+	local ptr, sz = bcfx.MakeUintArray(indexTbl, #indexTbl)
+	local idxHandle = bcfx.createIndexBuffer(ptr, sz)
+	return idxHandle
+end
+
+local vsStr = [[
+#version 410 core
+in vec3 a_position;
+in vec3 a_color0;
+
+out vec3 v_color0;
+
+void main() {
+	gl_Position = vec4(a_position, 1.0);
+	v_color0 = a_color0;
+}
+]]
+local fsStr = [[
+#version 410 core
+in vec3 v_color0;
+
+out vec4 FragColor;
+
+void main() {
+	FragColor = vec4(v_color0, 1.0);
+}
+]]
+local function CreateShaderProgram()
+	local vertexShaderHandle = bcfx.createShader(vsStr, shader_type.Vertex)
+	local fragmentShaderHandle = bcfx.createShader(fsStr, shader_type.Fragment)
+	local shaderProgramHandle = bcfx.createProgram(vertexShaderHandle, fragmentShaderHandle)
+	return shaderProgramHandle
+end
+
+local function SetupViewFull(viewID, win, width, height)
+	bcfx.setViewWindow(viewID, win)
+	local color = bcfx.color.pack(51, 76, 76, 255)
+	bcfx.setViewClear(viewID, clear_flag.COLOR, color, 0.0, 0)
+	bcfx.setViewRect(viewID, 0, 0, width, height)
+end
+local function SetupViewHalf(viewID, win, width, height)
+	bcfx.setViewWindow(viewID, win)
+	local color = bcfx.color.pack(199, 174, 174, 255)
+	bcfx.setViewClear(viewID, clear_flag.COLOR, color, 0.0, 0)
+	bcfx.setViewRect(viewID, 0, 0, width, height)
+end
+local function SetupAnotherView(viewID, mainWin)
+	local win = glfw.CreateWindow(800, 600, "Another", nil, mainWin)
+	if not win then
+		print("GLFW CreateWindow Error:", glfw.GetError())
+		glfw.Terminate()
+		return
+	end
+
+	bcfx.setViewWindow(viewID, win)
+	bcfx.setViewRect(viewID, 0, 0, glfw.GetFramebufferSize(win))
+	glfw.SetFramebufferSizeCallback(win, function(window, width, height)
+		bcfx.setViewRect(viewID, 0, 0, width, height)
+	end)
+end
+
+local layout
+local layoutHandle
+local vertexHandle
+local colorLayout
+local colorLayoutHandle
+local colorHandle
+local idxHandle
+local shaderProgramHandle
+
+local COLOR
+
+local function setup(mainWin)
+	layout, layoutHandle, vertexHandle = CreateVertexBuffer()
+	colorLayout, colorLayoutHandle, colorHandle = CreateColorBuffer()
+	idxHandle = CreateIndexBuffer()
+	shaderProgramHandle = CreateShaderProgram()
+
+	glfw.SetFramebufferSizeCallback(mainWin, function(window, width, height)
+		bcfx.setViewRect(0, 0, 0, width, height)
+		bcfx.setViewRect(1, 0, 0, width / 2, height / 2)
+	end)
+	local pixelw, pixelh = glfw.GetFramebufferSize(mainWin)
+	SetupViewFull(0, mainWin, pixelw, pixelh)
+	SetupViewHalf(1, mainWin, pixelw / 2, pixelh / 2)
+	SetupAnotherView(2, mainWin)
+
+	local timer = libuv.timer.new()
+	timer:start(function()
+		print("FrameRate:", GetFrameRate())
+	end, 1000, 1000)
+
+	COLOR = clear_flag.COLOR
+end
+
+local function tick(delta)
+	AddOneFrame(delta)
+
+	bcfx.setVertexBuffer(0, vertexHandle)
+	bcfx.setVertexBuffer(1, colorHandle)
+	bcfx.setIndexBuffer(idxHandle)
+	bcfx.submit(0, shaderProgramHandle)
+
+	bcfx.setVertexBuffer(0, vertexHandle)
+	bcfx.setVertexBuffer(1, colorHandle)
+	bcfx.setIndexBuffer(idxHandle)
+	bcfx.submit(1, shaderProgramHandle)
+
+	bcfx.setVertexBuffer(0, vertexHandle)
+	bcfx.setVertexBuffer(1, colorHandle)
+	bcfx.setIndexBuffer(idxHandle)
+	bcfx.submit(2, shaderProgramHandle)
+
+	local color = bcfx.color.pack(0, 255, 0, 255)
+	bcfx.setViewClear(2, COLOR, color, 0.0, 0)
+end
+
+return {
+	setup = setup,
+	tick = tick,
+}
