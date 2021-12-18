fff34c17549b23255791fcae44a6a1932dc4b27e
zhyingkun <1464959348@qq.com>
2021-12-18 13:21:42 +0800

    Update lua module for bcfx

--- lmod/bcfx.lua
+++ lmod/bcfx.lua
@@ -11,9 +11,9 @@ local ASYNC_WAIT_MSG = "AsyncWait api must running in coroutine"
 local bcfx = {}
 
 function bcfx.writeFileAsync(fileName, mb, callback)
-	local ptr = mbio.PackWriteFileParam(fileName, mb)
-	libuv.queue_work(mbio.WriteFilePtr, ptr, function(status, result)
-		local ret = mbio.UnpackWriteFileResult(result)
+	local ptr = mbio.packWriteFileParam(fileName, mb)
+	libuv.queue_work(mbio.writeFilePtr, ptr, function(status, result)
+		local ret = mbio.unpackWriteFileResult(result)
 		callback(ret)
 	end)
 end
@@ -27,12 +27,12 @@ function bcfx.writeFileAsyncWait(fileName, mb)
 end
 
 function bcfx.imageEncode(mb, x, y, comp, type, sorq)
-	return image.ImageEncode(mb, x, y, comp, type, sorq)
+	return image.imageEncode(mb, x, y, comp, type, sorq)
 end
 function bcfx.imageEncodeAsync(mb, x, y, comp, type, sorq, callback) -- stride or quality
-	local ptr = image.PackImageEncodeParam(mb, x, y, comp, type, sorq);
-	libuv.queue_work(image.ImageEncodePtr, ptr, function(status, result)
-		local mb = image.UnpackImageEncodeResult(result)
+	local ptr = image.packImageEncodeParam(mb, x, y, comp, type, sorq);
+	libuv.queue_work(image.imageEncodePtr, ptr, function(status, result)
+		local mb = image.unpackImageEncodeResult(result)
 		callback(mb)
 	end)
 end
