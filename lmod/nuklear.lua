036d1aca7c0a04f544617c946e9dbaea7909db83
zhyingkun <1464959348@qq.com>
2021-12-26 21:02:02 +0800

    Update lua module for nuklear

--- lmod/nuklear.lua
+++ lmod/nuklear.lua
@@ -3,23 +3,23 @@ local libnk = require("libnuklear")
 local nk = {}
 
 function nk.FontConfig()
-	return libnk.font_config()
+	return libnk.FontConfig()
 end
 
 function nk.FontAtlas()
-	return libnk.font_atlas()
+	return libnk.FontAtlas()
 end
 
 function nk.Buffer()
-	return libnk.buffer()
+	return libnk.Buffer()
 end
 
 function nk.Context(font)
-	return libnk.context(font)
+	return libnk.Context(font)
 end
 
 function nk.Image(handle)
-	return libnk.image(handle)
+	return libnk.Image(handle)
 end
 
 local curCtx
@@ -39,6 +39,9 @@ end
 function nk.inputButton(button, x, y, down)
 	libnk.input_button(curCtx, button, x, y, down)
 end
+function nk.inputScroll(x, y)
+	libnk.input_scroll(curCtx, x, y)
+end
 function nk.inputEnd()
 	libnk.input_end(curCtx)
 end
@@ -51,8 +54,8 @@ function nk.begin(title, x, y, w, h, flags)
 	return libnk.begin(curCtx, title, x, y, w, h, flags)
 end
 
-function nk.endNk()
-	libnk.endnk(curCtx)
+function nk.endWindow()
+	libnk.end_window(curCtx)
 end
 
 function nk.layoutRowDynamic(height, cols)
@@ -63,8 +66,8 @@ function nk.layoutRowStatic(height, itemWidth, cols)
 	libnk.layout_row_static(curCtx, height, itemWidth, cols)
 end
 
-function nk.label(str, alignment)
-	libnk.label(curCtx, str, alignment)
+function nk.textWidget(str, alignment)
+	libnk.text_widget(curCtx, str, alignment)
 end
 
 function nk.layoutRow(fmt, height, cols, ratio)
@@ -75,16 +78,16 @@ function nk.spacing(cols)
 	libnk.spacing(curCtx, cols)
 end
 
-function nk.buttonLabel(title)
-	return libnk.button_label(curCtx, title)
+function nk.buttonText(title)
+	return libnk.button_text(curCtx, title)
 end
 
 function nk.buttonImage(img)
 	return libnk.button_image(curCtx, img)
 end
 
-function nk.buttonImageLabel(img, label, align)
-	return libnk.button_image_label(curCtx, img, label, align)
+function nk.buttonImageText(img, str, align)
+	return libnk.button_image_text(curCtx, img, str, align)
 end
 
 function nk.imageWidget(img)
@@ -112,23 +115,19 @@ function nk.convert(cmds, vbuf, ebuf, config)
 	libnk.convert(curCtx, cmds, vbuf, ebuf, config)
 end
 
-function nk.drawElements(cmds)
-	return function(cmds, cmd)
-		if cmd == nil then
-			cmd = libnk.draw_list_begin(curCtx, cmds)
-		else
-			cmd = libnk.draw_list_next(curCtx, cmds, cmd)
-		end
-		if cmd == NULL then return end
-		return cmd
-	end, cmds, nil
+function nk.drawForEach(cmds, screenWidth, screenHeight, pixelWidth, pixelHeight, callback)
+	libnk.drawForEach(curCtx, cmds, screenWidth, screenHeight, pixelWidth, pixelHeight, callback)
 end
-
-nk.draw_list_begin = libnk.draw_list_begin
-nk.draw_list_next = libnk.draw_list_next
-nk.UnpackDrawCommand = libnk.UnpackDrawCommand
 
 nk.panel_flag = libnk.panel_flag
 nk.text_alignment = libnk.text_alignment
+
+function nk.listViewBegin(idStr, flag, rowHeight, rowCount)
+	return libnk.list_view_begin(curCtx, idStr, flag, rowHeight, rowCount)
+end
+
+function nk.listViewEnd()
+	libnk.list_view_end()
+end
 
 return nk
