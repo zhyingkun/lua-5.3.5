5fbed3214952c7d7d5a0358ec504ec82d37507ee
zhyingkun <1464959348@qq.com>
2022-01-15 20:09:25 +0800

    Update libuv pipe wrap

--- cmod/uvwrap/wrap/handle/stream/pipe_wrap.c
+++ cmod/uvwrap/wrap/handle/stream/pipe_wrap.c
@@ -4,7 +4,7 @@
 #define PIPE_FUNCTION(name) UVWRAP_FUNCTION(pipe, name)
 #define PIPE_CALLBACK(name) UVWRAP_CALLBACK(pipe, name)
 
-static int PIPE_FUNCTION(new)(lua_State* L) {
+static int PIPE_FUNCTION(Pipe)(lua_State* L) {
   uv_loop_t* loop = luaL_checkuvloop(L, 1);
   int ipc = lua_toboolean(L, 2);
 
@@ -35,7 +35,7 @@ static int PIPE_FUNCTION(bind)(lua_State* L) {
   return 0;
 }
 
-static void PIPE_CALLBACK(connect)(uv_connect_t* req, int status) {
+static void PIPE_CALLBACK(connectAsync)(uv_connect_t* req, int status) {
   lua_State* L;
   PUSH_REQ_CALLBACK_CLEAN(L, req);
   UNHOLD_REQ_PARAM(L, req, 1);
@@ -43,16 +43,47 @@ static void PIPE_CALLBACK(connect)(uv_connect_t* req, int status) {
   lua_pushinteger(L, status);
   CALL_LUA_FUNCTION(L, 1, 0);
 }
-static int PIPE_FUNCTION(connect)(lua_State* L) {
+static int PIPE_FUNCTION(connectAsync)(lua_State* L) {
   uv_pipe_t* handle = luaL_checkpipe(L, 1);
   const char* name = luaL_checkstring(L, 2);
   luaL_checktype(L, 3, LUA_TFUNCTION);
 
   uv_connect_t* req = (uv_connect_t*)MEMORY_FUNCTION(malloc_req)(sizeof(uv_connect_t));
-  uv_pipe_connect(req, handle, name, PIPE_CALLBACK(connect));
+  uv_pipe_connect(req, handle, name, PIPE_CALLBACK(connectAsync));
   SET_REQ_CALLBACK(L, 3, req);
   HOLD_REQ_PARAM(L, req, 1, 2);
   return 0;
+}
+
+static void PIPE_CALLBACK(connectAsyncWait)(uv_connect_t* req, int status) {
+  lua_State* L;
+  GET_REQ_LUA_STATE(L, req);
+  lua_State* co = (lua_State*)uv_req_get_data(req);
+  (void)MEMORY_FUNCTION(free_req)(req);
+  lua_pushinteger(co, status);
+  int ret = lua_resume(co, L, 1);
+  if (ret != LUA_OK && ret != LUA_YIELD) {
+    fprintf(stderr, "connectAsyncWait resume coroutine error: %s", lua_tostring(co, -1));
+  }
+  UNHOLD_REQ_PARAM(co, req, 0);
+  UNHOLD_REQ_PARAM(co, req, 1);
+  UNHOLD_REQ_PARAM(co, req, 2);
+}
+#define ASYNC_WAIT_MSG "AsyncWait api must running in coroutine"
+static int PIPE_FUNCTION(connectAsyncWait)(lua_State* co) {
+  if (lua_pushthread(co)) {
+    return luaL_error(co, ASYNC_WAIT_MSG);
+  }
+  uv_pipe_t* handle = luaL_checkpipe(co, 1);
+  const char* name = luaL_checkstring(co, 2);
+
+  uv_connect_t* req = (uv_connect_t*)MEMORY_FUNCTION(malloc_req)(sizeof(uv_connect_t));
+  uv_req_set_data(req, (void*)co);
+  uv_pipe_connect(req, handle, name, PIPE_CALLBACK(connectAsyncWait));
+  HOLD_REQ_PARAM(co, req, 0, -1);
+  HOLD_REQ_PARAM(co, req, 1, 1);
+  HOLD_REQ_PARAM(co, req, 2, 2);
+  return lua_yield(co, 0);
 }
 
 static int PIPE_FUNCTION(getsockname)(lua_State* L) {
@@ -112,7 +143,8 @@ static int PIPE_FUNCTION(__gc)(lua_State* L) {
 static const luaL_Reg PIPE_FUNCTION(metafuncs)[] = {
     EMPLACE_PIPE_FUNCTION(open),
     EMPLACE_PIPE_FUNCTION(bind),
-    EMPLACE_PIPE_FUNCTION(connect),
+    EMPLACE_PIPE_FUNCTION(connectAsync),
+    EMPLACE_PIPE_FUNCTION(connectAsyncWait),
     EMPLACE_PIPE_FUNCTION(getsockname),
     EMPLACE_PIPE_FUNCTION(getpeername),
     EMPLACE_PIPE_FUNCTION(pending_instances),
@@ -136,7 +168,7 @@ static void PIPE_FUNCTION(init_metatable)(lua_State* L) {
 }
 
 static const luaL_Reg PIPE_FUNCTION(funcs)[] = {
-    EMPLACE_PIPE_FUNCTION(new),
+    EMPLACE_PIPE_FUNCTION(Pipe),
     {NULL, NULL},
 };
 
