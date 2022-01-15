b0713b1d7549c4e867c53aab8f05f027728a2ed2
zhyingkun <1464959348@qq.com>
2022-01-15 12:08:04 +0800

    Update dir lua module

---
+++ lmod/dir.lua
@@ -0,0 +1,56 @@
+local libdir = require("libdir")
+
+---@class dir
+local dir = {}
+
+---@alias DirIterSignature fun():string, boolean
+
+---@param path string
+---@return DirIterSignature
+function dir.open(path)
+	return libdir.open(path)
+end
+---@param path string
+---@return DirIterSignature
+function dir.dirs(path)
+	return libdir.dirs(path)
+end
+---@overload fun(path:string):boolean, string, integer
+---@overload fun(path:string, onlyOne:boolean):boolean, string, integer
+---@param path string
+---@param onlyOne boolean
+---@return boolean, string, integer @errStr, errCode
+function dir.mkdir(path, onlyOne)
+	return libdir.mkdir(path, onlyOne)
+end
+---@param path string
+---@return boolean, string, integer @errStr, errCode
+function dir.rmdir(path)
+	return libdir.rmdir(path)
+end
+---@return string | nil, errStr, errCode
+function dir.cwd()
+	return libdir.cwd()
+end
+---@param path string
+---@return boolean, string, integer @errStr, errCode
+function dir.chdir(path)
+	return libdir.chdir(path)
+end
+---@param path string
+---@return string
+function dir.abspath(path)
+	return libdir.abspath(path)
+end
+---@param path string
+---@return string
+function dir.dirname(path)
+	return libdir.dirname(path)
+end
+---@param path string
+---@return string
+function dir.basename(path)
+	return libdir.basename(path)
+end
+
+return dir
