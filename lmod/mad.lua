83f7e33a597a6fec04a1d27907e3283d838a3bd1
zhyingkun <1464959348@qq.com>
2022-02-08 22:39:24 +0800

    Update mad lua module

--- lmod/mad.lua
+++ lmod/mad.lua
@@ -1,1 +1,39 @@
-return require("libmadwrap")
+local libmad = require("libmadwrap")
+
+local libuv = require("libuv")
+local queueWorkAsync = libuv.queueWorkAsync
+local queueWorkAsyncWait = libuv.queueWorkAsyncWait
+
+---@class mad:table
+local mad = {}
+
+--[[
+** {======================================================
+** MAD
+** =======================================================
+--]]
+
+---@param mb luaL_MemBuffer
+---@return luaL_MemBuffer | nil, integer
+function mad.madDecode(mb)
+	return libmad.madDecode(mb)
+end
+---@param mb luaL_MemBuffer
+---@param callback fun(mb:luaL_MemBuffer | nil, errCode:integer):void
+function mad.madDecodeAsync(mb, callback)
+	local ptr = libmad.packMadDecodeParam(mb)
+	queueWorkAsync(libmad.madDecodePtr, ptr, function(result, status)
+		callback(libmad.unpackMadDecodeResult(result))
+	end)
+end
+---@param mb luaL_MemBuffer
+---@return luaL_MemBuffer | nil, integer
+function mad.madDecodeAsyncWait(mb)
+	local ptr = libmad.packMadDecodeParam(mb)
+	local result = queueWorkAsyncWait(libmad.madDecodePtr, ptr)
+	return libmad.unpackMadDecodeResult(result)
+end
+
+-- }======================================================
+
+return mad
