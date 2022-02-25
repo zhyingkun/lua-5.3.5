065a36b951f0969e362eac55dbe15a9a6aa90fe0
zhyingkun <1464959348@qq.com>
2022-02-25 22:02:12 +0800

    Update mad wrap for mp3

--- cmod/madwrap/wrap/mad_wrap.c
+++ cmod/madwrap/wrap/mad_wrap.c
@@ -12,6 +12,139 @@
 
 #include <mad.h>
 
+/*
+** {======================================================
+** id3tag
+** =======================================================
+*/
+
+enum {
+  ID3_TAG_FLAG_UNSYNCHRONISATION = 0x80,
+  ID3_TAG_FLAG_EXTENDEDHEADER = 0x40,
+  ID3_TAG_FLAG_EXPERIMENTALINDICATOR = 0x20,
+  ID3_TAG_FLAG_FOOTERPRESENT = 0x10,
+  ID3_TAG_FLAG_KNOWNFLAGS = 0xf0,
+};
+
+typedef enum {
+  TT_NONE = 0,
+  TT_ID3V1,
+  TT_ID3V2,
+  TT_ID3V2_FOOTER,
+} TagType;
+
+static TagType id3tag_type(const uint8_t* data, size_t len) {
+  if (len >= 3 &&
+      data[0] == 'T' &&
+      data[1] == 'A' &&
+      data[2] == 'G') {
+    return TT_ID3V1;
+  }
+  if (len >= 10 &&
+      ((data[0] == 'I' && data[1] == 'D' && data[2] == '3') ||
+       (data[0] == '3' && data[1] == 'D' && data[2] == 'I')) &&
+      data[3] < 0xff && data[4] < 0xff &&
+      data[6] < 0x80 && data[7] < 0x80 && data[8] < 0x80 && data[9] < 0x80) {
+    return data[0] == 'I' ? TT_ID3V2 : TT_ID3V2_FOOTER;
+  }
+  return TT_NONE;
+}
+
+static uint32_t id3tag_parseUint(const uint8_t* data, size_t bytes) {
+  assert(bytes >= 1 && bytes <= 4);
+
+  const uint8_t** ptr = &data;
+  uint32_t value = 0;
+  switch (bytes) {
+    case 4:
+      value = (value << 8) | *(*ptr)++; /* fall through */
+    case 3:
+      value = (value << 8) | *(*ptr)++; /* fall through */
+    case 2:
+      value = (value << 8) | *(*ptr)++; /* fall through */
+    case 1:
+      value = (value << 8) | *(*ptr)++; /* fall through */
+  }
+  return value;
+}
+
+static size_t id3tag_parseSyncSafe(const uint8_t* data, size_t bytes) {
+  assert(bytes == 4 || bytes == 5);
+
+  const uint8_t** ptr = &data;
+  size_t value = 0;
+  switch (bytes) {
+    case 5:
+      value = (value << 4) | (*(*ptr)++ & 0x0f); /* fall through */
+    case 4:
+      value = (value << 7) | (*(*ptr)++ & 0x7f);
+      value = (value << 7) | (*(*ptr)++ & 0x7f);
+      value = (value << 7) | (*(*ptr)++ & 0x7f);
+      value = (value << 7) | (*(*ptr)++ & 0x7f);
+  }
+  return value;
+}
+
+static void parse_header(const uint8_t* data, uint32_t* version, uint32_t* flags, size_t* size) {
+  data += 3;
+  *version = id3tag_parseUint(data, 2);
+  *flags = id3tag_parseUint(data + 2, 1);
+  *size = id3tag_parseSyncSafe(data + 2 + 1, 4);
+}
+
+// if a tag begins at the given location, return its size, else return zero
+static size_t id3tag_query(const uint8_t* data, size_t len) {
+  uint32_t version;
+  uint32_t flags;
+  size_t size;
+  switch (id3tag_type(data, len)) {
+    case TT_ID3V1:
+      return 128;
+    case TT_ID3V2:
+      parse_header(data, &version, &flags, &size);
+      if (flags & ID3_TAG_FLAG_FOOTERPRESENT) {
+        size += 10;
+      }
+      return 10 + size;
+    case TT_ID3V2_FOOTER:
+      parse_header(data, &version, &flags, &size);
+      return -size - 10;
+    case TT_NONE:
+      break;
+    default:
+      break;
+  }
+  return 0;
+}
+
+static const uint8_t* id3tag_findAudioData(const uint8_t* mp3, size_t len, size_t* size) {
+  size_t sz = 0;
+  if (len > 128) {
+    sz = id3tag_query(mp3 + len - 128, 128);
+    if (sz > 0) {
+      len -= sz;
+    }
+  }
+  // while (len > 0 && (sz = id3tag_query(mp3, len)) > 0) {
+  if (len > 0 && (sz = id3tag_query(mp3, len)) > 0) {
+    mp3 += sz;
+    len -= sz;
+  }
+  if (len > 10) {
+    sz = id3tag_query(mp3 + len - 10, 10);
+    if (sz > 0) {
+      len -= sz;
+    }
+  }
+  if (len > 0) {
+    *size = len;
+    return mp3;
+  }
+  return NULL;
+}
+
+/* }====================================================== */
+
 #define MADWRAP_FUNCTION(name) mad_wrap_##name
 
 typedef struct {
@@ -118,19 +251,27 @@ static void _releaseBuffer(void* ud, void* ptr) {
   free(ptr);
 }
 static int _madDecodeToBuffer(luaL_MemBuffer* in, luaL_MemBuffer* out) {
+  int err = 0;
   buffer buf[1];
-  BUFFER_INIT(buf, in->ptr, in->sz);
-  struct mad_decoder decoder[1];
-  mad_decoder_init(decoder, &buf, input, 0 /* header */, 0 /* filter */, output, error, 0 /* message */);
-  int err = mad_decoder_run(decoder, MAD_DECODER_MODE_SYNC);
-  mad_decoder_finish(decoder);
-  if (err == 0) {
-    MEMBUFFER_SET(out, (void*)buf->raw, buf->cnt, _releaseBuffer, NULL);
-    buf->raw = NULL;
+  size_t size = 0;
+  const uint8_t* ptr = id3tag_findAudioData(in->ptr, in->sz, &size);
+  if (ptr != NULL) {
+    BUFFER_INIT(buf, ptr, size);
+    struct mad_decoder decoder[1];
+    mad_decoder_init(decoder, &buf, input, 0 /* header */, 0 /* filter */, output, error, 0 /* message */);
+    err = mad_decoder_run(decoder, MAD_DECODER_MODE_SYNC);
+    mad_decoder_finish(decoder);
+    if (err == 0) {
+      MEMBUFFER_SET(out, (void*)buf->raw, buf->cnt, _releaseBuffer, NULL);
+      buf->raw = NULL;
+    } else {
+      MEMBUFFER_CLEAR(out);
+    }
+    BUFFER_FREE_RESULT(buf);
   } else {
+    err = -2;
     MEMBUFFER_CLEAR(out);
   }
-  BUFFER_FREE_RESULT(buf);
   MEMBUFFER_RELEASE(in);
   return err;
 }
@@ -151,7 +292,7 @@ static void* MADWRAP_FUNCTION(madDecodePtr)(void* arg) {
   return (void*)result;
 }
 static int _dealMadDecodeResult(lua_State* L, int err, luaL_MemBuffer* mb) {
-  if (err != 0) {
+  if (err == 0) {
     luaL_MemBuffer* newMB = luaL_newmembuffer(L);
     MEMBUFFER_MOVE(mb, newMB);
   } else {
