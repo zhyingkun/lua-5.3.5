dc2dff5d43db0810623762949ac936280a039d7f
zhyingkun <1464959348@qq.com>
2022-12-03 13:13:46 +0800

    Add demo script for debug lua parser

---
+++ demo/scripts/parse.lua
@@ -0,0 +1,54 @@
+local a, b, c = 43, "Hello", true
+local tbl = {
+    [1] = 66,
+    Nice = "Good",
+}
+tbl.World = {}
+function tbl.World:Famous(p)
+    for k, v in pairs(self) do
+        print(p, k, v)
+    end    
+end
+
+tbl.World:Famous {}
+tbl.World:Famous "Literal"
+
+do
+    print(a, b, c)
+end
+
+::JustDoIt::
+
+local total = tbl[1]
+for i = 1, 10, 1 do
+    if i == 3 then
+        break
+    elseif i < 2 then
+        total = total + i
+    else
+        total = total + i * 10
+    end
+end
+
+while c do
+    c = false
+    repeat
+        total = total - 100
+    until total < 1
+end
+
+local function DayDayUp(...)
+    local params = {...}
+    print(...)
+    return b
+end
+GoodGoodStudy = function(param, param2)
+    return DayDayUp(param, param2, c)
+end
+local ret = GoodGoodStudy("Lua", "Sun")
+print(ret)
+
+if tbl[1] == 66 then
+    tbl[1] = 88
+    goto JustDoIt
+end
