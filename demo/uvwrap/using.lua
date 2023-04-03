2ec99d09f975cd3b498617416f7b671451513cf0
zhyingkun <1464959348@qq.com>
2023-04-03 22:34:55 +0800

    Update libuv demo

--- demo/uvwrap/using.lua
+++ demo/uvwrap/using.lua
@@ -1,6 +1,8 @@
+local libuv = require("libuv")
+local fs = libuv.fs
+libuv.setLoop()
+
 local function copyfile(from, to)
-	local libuv = require("libuv")
-	local fs = libuv.fs
 	local open_flag = fs.open_flag
 	local fd_from = fs.open(from, open_flag.RDONLY, 0)
 	local fd_to = fs.open(to, open_flag.WRONLY | open_flag.CREAT, 6 * 64 + 4 * 8 + 4) -- 0o644
@@ -9,3 +11,24 @@ local function copyfile(from, to)
 	local len = fs.write(fd_to, str, -1)
 	fs.close(fd_to)
 end
+
+local function ScanDirectory()
+	local folder = "."
+	local fd, err = fs.openDir(folder)
+	if err ~= libuv.err_code.OK then
+		print("Error:", folder, libuv.errName(err), libuv.strError(err))
+		return
+	end
+	local pathList = setmetatable({}, table)
+	repeat
+		local relativePath, type, err = fs.readDir(fd)
+		print(relativePath, type, err)
+		if relativePath then
+			pathList:insert(relativePath)
+		end
+	until relativePath == nil
+	fs.closeDir(fd)
+	return pathList
+end
+
+print(tostring(fs.scanDir("demo/pbdemo/", true), 1))
